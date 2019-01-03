2ba6ba2ee41f33ea8fa1b74cfe1ef5184e4b1edd
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2019-01-03 13:39:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,22 @@
 $rawJSON = file_get_contents('php://input');
 $EchoReqObj = json_decode($rawJSON);
 if($EchoReqObj->queryResult->queryText =="test it"){
-  $array = array ('payload' =>array ('google' => array ('expectUserResponse' => true,'richResponse' => array ('items' => array (
-          array (
-            'simpleResponse' => 
-            array (
-              'textToSpeech' => 'this is a simple response',
-            ),
-          ),
+  $array = array (
+  'fulfillmentText' => ' ',
+  'fulfillmentMessages' => 
+  array (
+    0 => 
+    array (
+      'text' => 
+      array (
+        'text' => 
+        array (
+          0 => 'Right now its 47.07 degress with clear sky',
         ),
       ),
     ),
   ),
+  'source' => '',
 )
   echo json_encode($array);
  }
