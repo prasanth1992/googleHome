42317d62db4920400e3c6f7b8db625fc85a093a5
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2019-01-03 16:06:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->queryResult->intent->displayName;
+	$text = $json->result->metadata->intentName;
 
 	switch ($text) {
 		case 'welcome':
