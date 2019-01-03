7401d695539d087448e20590f16293dea6e834d2
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2019-01-03 13:28:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 $rawJSON = file_get_contents('php://input');
 $EchoReqObj = json_decode($rawJSON);
-print_r($EchoReqObj);
+if($EchoReqObj->queryResult->intent->displayName =="welcome"){
+  $array = array ('payload' =>array ('google' => array ('expectUserResponse' => true,'richResponse' => array ('items' => array (
+          array (
+            'simpleResponse' => 
+            array (
+              'textToSpeech' => 'this is a simple response',
+            ),
+          ),
+        ),
+      ),
+    ),
+  ),
+)
+  echo json_encode($array);
+ }
+
 
 
 ?>
